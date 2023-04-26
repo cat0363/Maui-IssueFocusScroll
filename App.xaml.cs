019d5059f91c8980b8fd2f3c0cb5219a79aaa6f5
cat0363 <kenji.nagano.0363@gmail.com>
2023-04-26 10:19:12 +0900

    Repro code of .NET MAUI ScrollView Issue on iOS

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui_IssueFocusScroll;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Maui_IssueFocusScroll;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<ViewModelTest> pVmTest = new ObservableCollection<ViewModelTest>();
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        for (int i = 0; i < 100; i++)
+        {
+            pVmTest.Add(new ViewModelTest() { No = i + 1 });
+        }
+        BindableLayout.SetItemsSource(slTest, pVmTest);
+    }
+}
+
+public class ViewModelTest
+{
+    public int No { get; set; }
+
+    public string ItemName { get; set; }
+}
